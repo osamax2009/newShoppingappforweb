06492e90d0085e4ae0a083f46e2ddd85f990666e
MSCosamaShallal <osamafalah85@yahoo.com>
2021-10-01 10:41:28 +0300

    Scaffold Widget part 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,12 @@
 import 'package:flutter/material.dart';
 
-void main() {
+import 'ShoppingScreen/ShoppingScreen.dart';
+
+void main(){
   runApp(
     MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          leading: Icon(
-            Icons.settings
-          ),
-          title: Text("my first app"),
-          actions: [
-            Icon(
-                Icons.notification_important_sharp
-            ),
-            Icon(
-                Icons.shopping_cart
-            ),
-            Icon(
-                Icons.favorite
-            ),
-          ],
-        ),
-      )
-     ),
+      debugShowCheckedModeBanner: false,
+      home: ShoppingScreen(),
+    )
   );
-
 }
