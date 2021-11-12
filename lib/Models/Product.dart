8065809442db26b4edc1eeacaa19fbd67145d746
MSCosamaShallal <osamafalah85@yahoo.com>
2021-11-12 17:02:13 +0300

    first commit

--- lib/Models/Product.dart
+++ lib/Models/Product.dart
@@ -1,18 +1,24 @@
 
 
-class Product {
-  String id ;
-  String title;
-  String description;
-  String imageUrl;
-  double price;
 
-  Product(this.id,
-      this.title,
-      this.description,
-      this.imageUrl,
-      this.price
-      );
+import 'package:flutter/foundation.dart';
+
+class Product {
+  String? id  ;
+  String? title;
+  String? description;
+  String? imageUrl;
+  double? price;
+  bool fav ;
+
+  Product({
+  @required  this.id,
+    @required    this.title,
+    @required   this.description,
+    @required    this.imageUrl,
+    @required     this.price,
+       this.fav = true
+    });
 
 
 
