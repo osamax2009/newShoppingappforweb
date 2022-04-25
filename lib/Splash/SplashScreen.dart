a7e7b4af0eb22f6790763eec1896ca12d2190419
MSCosamaShallal <osamafalah85@yahoo.com>
2022-04-25 22:37:48 +0300

    first commit

--- lib/Splash/SplashScreen.dart
+++ lib/Splash/SplashScreen.dart
@@ -1,74 +1,91 @@
-
-
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
-   SplashScreen({Key? key}) : super(key: key);
+  SplashScreen({Key? key}) : super(key: key);
   static String routeName = "splash";
-
 
   var listofSplash = [
     {
-      "text": "welcome to myshopping App",
-      "image" : "assets/image/image1.jpg"
+      "text": "Welcome to shopping, Let’s shop!",
+      "image": "assets/image/image1.jpg"
     },
     {
-      "text": " our app have great product",
-      "image" : "assets/image/image1.jpg"
+      "text":
+          "We help people conect with store \naround United State of America",
+      "image": "assets/image/image2.jpg"
+    },
+    {
+      "text": "We show the easy way to shop. \nJust stay at home with us",
+      "image": "assets/image/image3.jpg"
     }
   ];
 
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "SplashScreen"
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("SplashScreen"),
         ),
-      ),
-      body: Container(
-        width: double.infinity,
-        child:Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(13.0),
-              child: Text(
-                  "MyShpping \n App",
-                style: TextStyle(
-                  fontSize: 25,
-                  color: Colors.orange
+        body: Container(
+            width: double.infinity,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(13.0),
+                  child: Text(
+                    "MyShpping \n App",
+                    style: TextStyle(fontSize: 25, color: Colors.orange),
+                    textAlign: TextAlign.center,
+                  ),
                 ),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            
-            Expanded(
-              child: PageView.builder(
-                    itemCount: listofSplash.length,
-                  itemBuilder: (cx,index)=>
-                      Column(
-                        children: [
-                          Text(
-                              listofSplash[index]["text"]!,
-                            textAlign: TextAlign.center,
-                          ),
-                          Spacer(
-                            flex: 2,
-                          ),
-                          Image.asset(listofSplash[index]["image"]!,
-                            height: MediaQuery.of(context).size.height * 0.5,
-                            width: MediaQuery.of(context).size.width * 0.8,
-                          )
-                          
-                        ],
-                      )
-              ),
-            )
-
-          ],
-        )
+                Expanded(
+                  child: PageView.builder(
+                      itemCount: listofSplash.length,
+                      itemBuilder: (cx, index) => Column(
+                            children: [
+                              Text(
+                                listofSplash[index]["text"]!,
+                                textAlign: TextAlign.center,
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(20.0),
+                                child: Image.asset(
+                                  listofSplash[index]["image"]!,
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.3,
+                                  width: MediaQuery.of(context).size.width * 0.8,
+                                ),
+                              ),
+                               Spacer(),
+                              Container(
+                                margin: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                                width: double.infinity,
+                                height: 50,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(15)),
+                                child: ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                        primary: Colors.orange),
+                                    onPressed: () {},
+                                    child: Text("Continue...",
+                                        style: TextStyle(
+                                          fontSize: 18,
+                                          color: Colors.white,
+                                        ))),
+                              )
+                            ],
+                          )),
+                ),
+              ],
+            )),
       ),
     );
   }
+}
+
+
+Container buildContainer() {
+
+  re
 }
